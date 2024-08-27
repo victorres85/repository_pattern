74ac5ddd2dd08c3fa85b54e72a39bec4dc3899fa
victor-pi-almeida <victorres.almeida@gmail.com>
2024-08-27 11:32:23 +0100

    update

--- app/rest/parlamentar.py
+++ app/rest/parlamentar.py
@@ -1,31 +1,10 @@
 """ Parlamentar REST_API Endpoints """
 
-from flask import Blueprint, request, jsonify
-from app.infrastructure.repository.parlamentar import ParlamentarRepository
-from app.domain.parlamentar import ParlamentarDomain
-from app import db
+from flask import Blueprint
 
 parlamentar_api_blueprint = Blueprint("parlamentar", __name__)
 
 
-@parlamentar_api_blueprint.route("/parlamentar", methods=["GET", "POST"])  # type: ignore
+@parlamentar_api_blueprint.route("/parlamentar", methods=["GET", "POST"])
 def add_parlamentar():
-    if request.method == "GET":
-        parlamentar_id = request.args.get("id")
-        response = ParlamentarRepository().get(int(parlamentar_id))  # type: ignore
-        return jsonify({"data": response}), 200
-
-    if request.method == "POST":
-        parlamentar = ParlamentarDomain.model_validate_json(request.data)
-        repo = ParlamentarRepository().add(parlamentar)
-        return jsonify({"data": repo}), 201
-    return None
-
-
-@parlamentar_api_blueprint.route("/test_db")
-def test_db():
-    try:
-        db.session.execute("SELECT 1")  # type: ignore
-        return "Database connection successful!"
-    except Exception as e:
-        return str(e)
+    pass
