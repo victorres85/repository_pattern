962643c7144214a74eed5397c315eea90218568d
victor-pi-almeida <victorres.almeida@gmail.com>
2024-08-27 10:14:21 +0100

    delete test file

--- app/infrastructure/models/test.py
+++
@@ -1,7 +0,0 @@
-from app import db
-
-
-class Test(db.Model):
-    __tablename__ = "test"
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String, nullable=True)
